916fa6d4e2b390a9b433fc4fa4eb26e1cb16db58
Mateosacco99 <mateosacco99@gmail.com>
2026-04-18 00:22:02 -0300

    Fix: Posicionamiento barra reproduccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
+import { useContext } from "react"
 import NavBar from "./components/NavBar"
 import HeroSection from "./components/HeroSection"
 import Biografia from "./components/Biografia"
 import Proyectos from "./components/Proyectos"
 import GlobalMusicPlayer from "./components/GlobalMusicPlayer"
-import { MusicProvider } from "./context/MusicContext"
+import { MusicProvider, MusicContext } from "./context/MusicContext"
 import Contacto from "./components/Contacto"
 import ErrorBoundary from "./components/ErrorBoundary"
+
+const AppContent = () => {
+  const { currentTrack, playlist } = useContext(MusicContext)
+  const playerActive = currentTrack && playlist.length > 0
+
+  return (
+    <div className={`app-content ${playerActive ? 'player-active' : ''}`}>
+      <NavBar />
+      <HeroSection />
+      <Biografia />
+      <Proyectos />
+      <Contacto />
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <ErrorBoundary>
       <MusicProvider>
-        <div>
-          <NavBar />
-          <HeroSection />
-          <Biografia />
-          <Proyectos />
-          <Contacto />
-          <GlobalMusicPlayer />
-        </div>
+        <AppContent />
+        <GlobalMusicPlayer />
       </MusicProvider>
     </ErrorBoundary>
   )
